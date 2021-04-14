30f8b6b6f3c3965c8539702cf14e4441379efad2
cess <270017772@qq.com>
2021-04-14 16:19:55 +0800

    提交

--- day09/src/com/atguigu/java2/RecursionTest.java
+++ day09/src/com/atguigu/java2/RecursionTest.java
@@ -4,7 +4,7 @@ package com.atguigu.java2;
  * 递归方法的使用（了解）
  * 1.递归方法：一个方法体内调用它自身。
  * 2.方法递归包含了一种隐式的循环，它会重复执行某段代码，但这种重复执行无须循环控制。
- * 递归一定要向已知方向递归，否则这种递归就变成了无穷递归，类似于死循环。
+ *   递归一定要向已知方向递归，否则这种递归就变成了无穷递归，类似于死循环。
  */
 
 public class RecursionTest {
@@ -18,6 +18,7 @@ public class RecursionTest {
             sum += i;
         }
         System.out.println(sum);
+
         // 方式二：
         RecursionTest test = new RecursionTest();
         int sum1 = test.getSum(100);
@@ -31,24 +32,20 @@ public class RecursionTest {
 
     // 例1：计算1-n之间所有自然数的和
     public int getSum(int n) {// 3
-
         if (n == 1) {
             return 1;
         } else {
             return n + getSum(n - 1);
         }
-
     }
 
     // 例2：计算1-n之间所有自然数的乘积:n!
     public int getSum1(int n) {
-
         if (n == 1) {
             return 1;
         } else {
             return n * getSum1(n - 1);
         }
-
     }
 
     //例3：已知有一个数列：f(0) = 1,f(1) = 4,f(n+2)=2*f(n+1) + f(n),
