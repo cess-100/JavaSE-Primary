e7b3fbbfdb9f17b0dddef1a588c23514f8b8d745
cess <270017772@qq.com>
2021-04-17 22:40:11 +0800

    提交

--- day12/src/com/atguigu/java4/Person.java
+++ day12/src/com/atguigu/java4/Person.java
@@ -1,17 +1,15 @@
 package com.atguigu.java4;
 
 public class Person {
-	String name;
-	int age;
-	
-	int id = 1001;
-	
-	public void eat(){
-		System.out.println("人：吃饭");
-	}
-	
-	public void walk(){
-		System.out.println("人：走路");
-	}
-	
+    String name;
+    int age;
+    int id = 1001;
+
+    public void eat() {
+        System.out.println("人：吃饭");
+    }
+
+    public void walk() {
+        System.out.println("人：走路");
+    }
 }
