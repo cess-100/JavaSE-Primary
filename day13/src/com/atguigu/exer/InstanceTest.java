85d541f803a038219bc144f07c9860e328101a9e
cess <270017772@qq.com>
2021-04-18 16:07:08 +0800

    提交

--- day13/src/com/atguigu/exer/InstanceTest.java
+++ day13/src/com/atguigu/exer/InstanceTest.java
@@ -13,28 +13,21 @@ package com.atguigu.exer;
 		如果e为Graduate类的对象，输出： 
 		“a graduated student”
 		“a student”
-		“a person” 
-
- * 
- * 
- * 
+		“a person”
  */
 public class InstanceTest {
-	
-	public static void main(String[] args) {
-		
-		InstanceTest test = new InstanceTest();
-		test.method(new Student());
-	}
-	
-	
-	public void method(Person e){
-		
-		//虚拟方法调用
-		String info = e.getInfo();
-		System.out.println(info);
-		
-		//方式一
+
+    public static void main(String[] args) {
+        InstanceTest test = new InstanceTest();
+        test.method(new Student());
+    }
+
+    public void method(Person e) {
+        //虚拟方法调用
+        String info = e.getInfo();
+        System.out.println(info);
+
+        //方式一
 //		if(e instanceof Graduate){
 //			System.out.println("a graduated student");
 //			System.out.println("a student");
@@ -45,45 +38,43 @@ public class InstanceTest {
 //		}else{
 //			System.out.println("a person");
 //		}
-		
-		//方式二
-		if(e instanceof Graduate){
-			System.out.println("a graduated student");
-		}
-		
-		if(e instanceof Student){
-			System.out.println("a student");
-		}
-		
-		if(e instanceof Person){
-			System.out.println("a person");
-		}
-		
-		
-	}
+
+        //方式二
+        if (e instanceof Graduate) {
+            System.out.println("a graduated student");
+        }
+
+        if (e instanceof Student) {
+            System.out.println("a student");
+        }
+
+        if (e instanceof Person) {
+            System.out.println("a person");
+        }
+    }
 }
 
 class Person {
-	protected String name = "person";
-	protected int age = 50;
+    protected String name = "person";
+    protected int age = 50;
 
-	public String getInfo() {
-		return "Name: " + name + "\n" + "age: " + age;
-	}
+    public String getInfo() {
+        return "Name: " + name + "\n" + "age: " + age;
+    }
 }
 
 class Student extends Person {
-	protected String school = "pku";
+    protected String school = "pku";
 
-	public String getInfo() {
-		return "Name: " + name + "\nage: " + age + "\nschool: " + school;
-	}
+    public String getInfo() {
+        return "Name: " + name + "\nage: " + age + "\nschool: " + school;
+    }
 }
 
 class Graduate extends Student {
-	public String major = "IT";
+    public String major = "IT";
 
-	public String getInfo() {
-		return "Name: " + name + "\nage: " + age + "\nschool: " + school + "\nmajor:" + major;
-	}
+    public String getInfo() {
+        return "Name: " + name + "\nage: " + age + "\nschool: " + school + "\nmajor:" + major;
+    }
 }
--- day13/src/com/atguigu/exer/InterviewTest1.java
+++ day13/src/com/atguigu/exer/InterviewTest1.java
@@ -3,29 +3,28 @@ package com.atguigu.exer;
 //考查多态的笔试题目：
 public class InterviewTest1 {
 
-	public static void main(String[] args) {
-		Base1 base = new Sub1();
-		base.add(1, 2, 3);
+    public static void main(String[] args) {
+        Base1 b = new Sub1();
+        b.add(1, 2, 3);
 
-		Sub1 s = (Sub1)base;
-		s.add(1,2,3);
-	}
+        Sub1 s = (Sub1) b;
+        s.add(1, 2, 3);
+    }
 }
 
 class Base1 {
-	public void add(int a, int... arr) {
-		System.out.println("base1");
-	}
+    public void add(int a, int... arr) {
+        System.out.println("base1");
+    }
 }
 
 class Sub1 extends Base1 {
 
-	public void add(int a, int[] arr) {
-		System.out.println("sub_1");
-	}
+    public void add(int a, int[] arr) {
+        System.out.println("sub_1");
+    }
 
-	public void add(int a, int b, int c) {
-		System.out.println("sub_2");
-	}
-
+    public void add(int a, int b, int c) {
+        System.out.println("sub_2");
+    }
 }
