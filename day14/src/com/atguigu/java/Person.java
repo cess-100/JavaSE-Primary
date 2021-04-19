262ad9061bc42451c7b0a2ca4cc55596e68487b1
cess <270017772@qq.com>
2021-04-19 22:23:43 +0800

    提交

--- day14/src/com/atguigu/java/Person.java
+++ day14/src/com/atguigu/java/Person.java
@@ -1,41 +1,41 @@
 package com.atguigu.java;
 
 public class Person {
-	String name;
-	int age;
+    String name;
+    int age;
 
-	public Person(String name, int age) {
-		super();
-		this.name = name;
-		this.age = age;
-	}
+    public Person(String name, int age) {
+        super();
+        this.name = name;
+        this.age = age;
+    }
 
-	public void eat() {
-		System.out.println("人：吃饭");
-	}
+    public void eat() {
+        System.out.println("人：吃饭");
+    }
 
-	public void walk() {
-		System.out.println("人：走路");
-	}
+    public void walk() {
+        System.out.println("人：走路");
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Person other = (Person) obj;
-		if (age != other.age)
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		return true;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Person other = (Person) obj;
+        if (age != other.age)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
 
 //	public boolean equals(Object obj) {
 //		if (obj == this) {
