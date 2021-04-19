2dae91aa7052ab5ac24528f567048845efb16a06
cess <270017772@qq.com>
2021-04-20 00:00:29 +0800

    提交

--- day14/src/com/atguigu/exer/AccountTest.java
+++ day14/src/com/atguigu/exer/AccountTest.java
@@ -1,9 +1,7 @@
 package com.atguigu.exer;
 
 public class AccountTest {
-	
 	public static void main(String[] args) {
-		
 		Account acct1 =  new Account();
 		Account acct2 =  new Account("qwerty",2000);
 		
@@ -16,5 +14,4 @@ public class AccountTest {
 		System.out.println(acct1.getInterestRate());
 		System.out.println(acct1.getMinMoney());
 	}
-	
 }
