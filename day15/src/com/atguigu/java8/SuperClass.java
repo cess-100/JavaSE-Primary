cc73691cc46b4a7f6e6070290a477916c77a121f
cess <270017772@qq.com>
2021-04-21 21:42:36 +0800

    提交

--- day15/src/com/atguigu/java8/SuperClass.java
+++ day15/src/com/atguigu/java8/SuperClass.java
@@ -1,9 +1,9 @@
 package com.atguigu.java8;
 
 public class SuperClass {
-	
-	public void method3(){
-		System.out.println("SuperClass:北京");
-	}
-	
+
+    public void method3() {
+        System.out.println("SuperClass:北京");
+    }
+
 }
